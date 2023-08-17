e0e2b7ce0d74938fe638691f6fc1bd5eb45c190a
YBRpcBrian <yabiunbrian1@gmail.com>
2023-08-17 23:48:25 +0100

    The Firstommit For Script Git Push

---
+++ GitPush.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#This Script Wil push Commits To The Remote Repository
+
+echo " What is The Alias Of Your Remote Repository URL "
+read alias
+yes=yes
+no=no
+echo " You Are About To Push commits To Alias == $alias (Remote Repository) "
+echo " IF You Want To Do This Action Type yes "
+read ans
+	if [[ $ans == $yes ]]
+	then
+echo " Pushing... "
+git push $alias master > GitPushLog.txt
+sleep 3
+echo " ++++++Push Was Successfull+++++++ "
+echo " You can Check The Push Logs In GitPushLog.txt "
+	elif [[ $ans == $no ]]
+	then
+echo "=====>Aborting Operation<====="
+sleep 2
+echo "-------Operation Aborted--------"
+	else
+echo " Input 'yes' OR 'no' "
+	fi
